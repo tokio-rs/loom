5879f1cdc12d6c01142345919ca36abdd54b7c30
Carl Lerche <me@carllerche.com>
2019-02-19 13:14:25 -0800

    Provide Arc implementation
    This is needed when `CausalCell` instances are accessed on drop.

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,10 +1,12 @@
 //! Mock implementation of `std::atomic`.
 
+mod arc;
 mod causal;
 pub mod atomic;
 mod condvar;
 mod mutex;
 
+pub use self::arc::Arc;
 pub use self::causal::CausalCell;
 pub use self::condvar::{Condvar, WaitTimeoutResult};
 pub use self::mutex::{Mutex, MutexGuard};
