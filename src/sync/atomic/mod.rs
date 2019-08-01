b5d419a7b6f36947a874f29e2ad29381020666ef
Josh Mcguigan <joshmcg88@gmail.com>
2019-08-01 04:16:57 -0700

    implement atomic int macro and add pub type AtomicU64

--- src/sync/atomic/mod.rs
+++ src/sync/atomic/mod.rs
@@ -1,15 +1,15 @@
 //! Mock implementation of `std::sync::atomic`.
 
 mod atomic;
+mod int;
 mod ptr;
-mod u32;
-mod usize;
 
 use self::atomic::Atomic;
 
+pub use self::int::AtomicU32;
+pub use self::int::AtomicU64;
+pub use self::int::AtomicUsize;
 pub use self::ptr::AtomicPtr;
-pub use self::u32::AtomicU32;
-pub use self::usize::AtomicUsize;
 
 /// Signals the processor that it is entering a busy-wait spin-loop.
 pub fn spin_loop_hint() {
