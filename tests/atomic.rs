0e1cc80bfa955645514c9b073861d8c0c311f8d2
Philip Hayes <philiphayes9@gmail.com>
2019-09-03 17:24:14 -0700

    [WIP] Add failing test case: atomic load on drop after panic

--- tests/atomic.rs
+++ tests/atomic.rs
@@ -40,3 +40,39 @@ fn invalid_get_mut() {
         let _ = unsafe { *(*v1.get()).get_mut() };
     });
 }
+
+#[test]
+#[should_panic]
+fn atomic_load_on_drop_in_panic_crashes() {
+    struct AtomicLoadOnDrop(AtomicUsize);
+
+    impl AtomicLoadOnDrop {
+        fn new() -> Self {
+            Self(AtomicUsize::new(0))
+        }
+    }
+
+    impl Drop for AtomicLoadOnDrop {
+        fn drop(&mut self) {
+            let _ = self.0.load(SeqCst);
+        }
+    }
+
+    loom::model(|| {
+        let a = AtomicLoadOnDrop::new();
+
+        // Moving `AtomicLoadOnDrop` into a thread will trigger `drop` when the
+        // thread is dropped.
+        thread::spawn(move || {
+            let _a = a;
+        });
+
+        // Panic the parent thread.
+        //
+        // Without a fix, the `AtomicLoadOnDrop` drops _after_ the
+        // `loom::rt::scheduler::STATE` thread local is dropped. Since atomics
+        // use `STATE` to track accesses, dropping `AtomicLoadOnDrop` causes an
+        // access to an unset `RefCell`.
+        panic!();
+    });
+}
