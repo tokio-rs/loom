0ea5b02cc3119d6968e893995486db86e4c0c8ef
Eliza Weisman <eliza@buoyant.io>
2019-09-24 14:53:24 -0700

    add test that thread locals are dropped
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tests/thread_local.rs
+++ tests/thread_local.rs
@@ -1,6 +1,7 @@
 #![deny(warnings, rust_2018_idioms)]
 use loom::thread;
 use std::cell::RefCell;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[test]
 fn thread_local() {
@@ -44,4 +45,45 @@ fn nested_with() {
     loom::model(|| {
         LOCAL1.with(|local1| *local1.borrow_mut() = LOCAL2.with(|local2| local2.borrow().clone()));
     });
+}
+
+#[test]
+fn drop() {
+    static DROPS: AtomicUsize = AtomicUsize::new(0);
+    #[allow(dead_code)]
+    struct CountDrops(&'static AtomicUsize);
+
+    impl Drop for CountDrops {
+        fn drop(&mut self) {
+            self.0.fetch_add(1, Ordering::Release);
+        }
+    }
+
+    loom::thread_local! {
+        static DROPPED_LOCAL: CountDrops = CountDrops(&DROPS);
+    }
+
+    loom::model(|| {
+        assert_eq!(DROPS.load(Ordering::Acquire), 0);
+
+        thread::spawn(|| assert_eq!(DROPS.load(Ordering::Acquire), 0))
+            .join()
+            .unwrap();
+
+        // When the first spawned thread completed, its copy of the thread local
+        // should have been dropped.
+        assert_eq!(DROPS.load(Ordering::Acquire), 1);
+
+        thread::spawn(|| assert_eq!(DROPS.load(Ordering::Acquire), 1))
+            .join()
+            .unwrap();
+
+        // When the second spawned thread completed, its copy of the thread local
+        // should have been dropped as well.
+        assert_eq!(DROPS.load(Ordering::Acquire), 2);
+    });
+
+    // Finally, when the model's "main thread" completes, its copy of the local
+    // should also be dropped.
+    assert_eq!(DROPS.load(Ordering::Acquire), 3);
 }
