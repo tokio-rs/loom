e6cc3ab52ba7d044cc47e2e47560b05bf6e7f8e8
Carl Lerche <me@carllerche.com>
2019-09-24 11:28:15 -0700

    Expose Notify (#60)
    This type is useful for mocking out primitives when writing tests.

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -5,10 +5,12 @@ pub mod atomic;
 mod causal;
 mod condvar;
 mod mutex;
+mod notify;
 
 pub use self::arc::Arc;
 pub use self::causal::CausalCell;
 pub use self::condvar::{Condvar, WaitTimeoutResult};
 pub use self::mutex::{Mutex, MutexGuard};
+pub use self::notify::Notify;
 
 pub use std::sync::{LockResult, TryLockResult};
