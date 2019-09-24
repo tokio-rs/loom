b180a8259dc338dec4e95dc247c0ce1d463bac14
hawkw <eliza@buoyant.io>
2019-09-24 14:29:23 -0700

    add test for nested thread_local accesses
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tests/thread_local.rs
+++ tests/thread_local.rs
@@ -7,48 +7,41 @@ fn thread_local() {
     loom::thread_local! {
         static THREAD_LOCAL: RefCell<usize> = RefCell::new(1);
     }
+
+    fn do_test(n: usize) {
+        THREAD_LOCAL.with(|local| {
+            assert_eq!(*local.borrow(), 1);
+        });
+        THREAD_LOCAL.with(|local| {
+            assert_eq!(*local.borrow(), 1);
+            *local.borrow_mut() = n;
+            assert_eq!(*local.borrow(), n);
+        });
+        THREAD_LOCAL.with(|local| {
+            assert_eq!(*local.borrow(), n);
+        });
+    }
+
     loom::model(|| {
-        let t1 = thread::spawn(|| {
-            THREAD_LOCAL.with(|local| {
-                assert_eq!(*local.borrow(), 1);
-            });
-            THREAD_LOCAL.with(|local| {
-                assert_eq!(*local.borrow(), 1);
-                *local.borrow_mut() = 2;
-                assert_eq!(*local.borrow(), 2);
-            });
-            THREAD_LOCAL.with(|local| {
-                assert_eq!(*local.borrow(), 2);
-            });
-        });
+        let t1 = thread::spawn(|| do_test(2));
 
-        let t2 = thread::spawn(|| {
-            THREAD_LOCAL.with(|local| {
-                assert_eq!(*local.borrow(), 1);
-            });
-            THREAD_LOCAL.with(|local| {
-                assert_eq!(*local.borrow(), 1);
-                *local.borrow_mut() = 3;
-                assert_eq!(*local.borrow(), 3);
-            });
-            THREAD_LOCAL.with(|local| {
-                assert_eq!(*local.borrow(), 3);
-            });
-        });
+        let t2 = thread::spawn(|| do_test(3));
 
-        THREAD_LOCAL.with(|local| {
-            assert_eq!(*local.borrow(), 1);
-        });
-        THREAD_LOCAL.with(|local| {
-            assert_eq!(*local.borrow(), 1);
-            *local.borrow_mut() = 4;
-            assert_eq!(*local.borrow(), 4);
-        });
-        THREAD_LOCAL.with(|local| {
-            assert_eq!(*local.borrow(), 4);
-        });
+        do_test(4);
 
         t1.join().unwrap();
         t2.join().unwrap();
+    });
+}
+
+#[test]
+fn nested_with() {
+    loom::thread_local! {
+        static LOCAL1: RefCell<usize> = RefCell::new(1);
+        static LOCAL2: RefCell<usize> = RefCell::new(2);
+    }
+
+    loom::model(|| {
+        LOCAL1.with(|local1| *local1.borrow_mut() = LOCAL2.with(|local2| local2.borrow().clone()));
     });
 }
