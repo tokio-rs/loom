86601d26890591f7277e1fd6a4b63101ca24707e
Carl Lerche <me@carllerche.com>
2019-10-15 22:01:56 -0700

    Release v0.2.10

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/loom/0.2.9")]
+#![doc(html_root_url = "https://docs.rs/loom/0.2.10")]
 #![deny(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 #![cfg_attr(test, deny(warnings))]
 
